529e63dfbd8a68ef64d40f07adf422abe4c9971d
Fabio Zanetti <fczanetti@hotmail.com>
2023-10-17 13:54:28 -0300

    add workflow - test02 - linha maior que 120 ch.

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -3,7 +3,7 @@ import requests
 
 def buscar_avatar(usuario):
     """
-    Busca o avatar de um usuário no GitHub.
+    Busca o avatar de um usuário no GitHub. kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
     :param usuario: Nome do usuário (string);
     :return: Avatar do usuário informado.
     """
