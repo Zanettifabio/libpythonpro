f5d71d1c66fed52cab22a3c4a4558790eff8cd7d
Fabio Zanetti <fczanetti@hotmail.com>
2023-10-27 10:37:48 -0300

    numero de emails enviados, parametrizacao de usuarios no teste de quant. spam e outros detalhes incrementados

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,14 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Fabio')
+    usuario = Usuario(nome='Fabio', email='fczanetti@hotmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuario(sessao):
-    usuarios = [Usuario(nome='Fabio'), Usuario(nome='Fernanda')]
+    usuarios = [Usuario(nome='Fabio', email='fczanetti@hotmail.com'),
+                Usuario(nome='Fernanda', email='fernanda@hotmail.com')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
