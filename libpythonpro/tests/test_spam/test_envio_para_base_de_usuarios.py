f143e5c98fc740b16a85e2e7f800c7e67eaf6f95
Fabio Zanetti <fczanetti@hotmail.com>
2023-10-27 13:16:52 -0300

    classe EnviadorMock criada e classe Enviador retornada ao seu formato original

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -19,10 +19,36 @@ from libpythonpro.spam.modelos import Usuario
 def test_qtd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'fczanetti@hotmail.com',
         'Curso Python Pro',
         'Confira os módulos')
     assert len(usuarios) == enviador.qtd_emails_enviados
+
+
+class EnviadorMock(Enviador):
+    def __init__(self):
+        self.parametros_de_envio = None
+        self.qtd_emails_enviados = 0
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_emails_enviados += 1
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Fabio', email='fczanetti@hotmail.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'fernanda@hotmail.com',
+        'Curso Python Pro',
+        'Confira os módulos')
+    assert enviador.parametros_de_envio == (
+        'fernanda@hotmail.com',
+        'fczanetti@hotmail.com',
+        'Curso Python Pro',
+        'Confira os módulos')
